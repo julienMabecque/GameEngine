7a71e565a5fec857d095e5a80ee4b63b8a500916
julienMabecque <julien.mabecque@hotmail.com>
2024-07-13 14:41:40 +0200

    pixel shader step

--- AppWindow.cpp
+++ AppWindow.cpp
@@ -34,10 +34,10 @@ void AppWindow::onCreate()
 
 	vertex list[] =
 	{
-		{ -0.5f, -0.5f, 0.0f,   1,0,0 },
-		{ -0.5f, 0.5f, 0.0f,    0,1,0 },
+		{ -0.5f, -0.5f, 0.0f,   0,0,0 },
+		{ -0.5f, 0.5f, 0.0f,    1,1,0 },
 		{ 0.5f, -0.5f, 0.0f,    0,0,1 },
-		{ 0.5f, 0.5f, 0.0f,     1,1,0 }
+		{ 0.5f, 0.5f, 0.0f,     1,1,1 }
 	};
 
 	m_vb=GraphicsEngine::get()->createVertexBuffer();
